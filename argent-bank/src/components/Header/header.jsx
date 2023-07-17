6404c37fd259227502adea530c2f27ea24fd53f6
Milla22 <marti.ludmilla@orange.fr>
2023-07-17 08:30:53 +0200

    Création du router

---
+++ argent-bank/src/components/Header/header.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import Logo from '../../assets/img/argentBankLogo.png';
+import {Navlink} from 'react-router-dom';
+
+
+import '../../index.css';
+
+function Header() {
+
+}
+export default Header;
---
+++ argent-bank/src/router/router.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from "../pages/Home/home";
+import Error from "../pages/Erreur/erreur";
+import Account from "../pages/Account/account";
+import Login from "../pages/Login/login";
+import Logout from "../pages/Logout/logout";
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+const Router = () => {
+return (
+<Routes>
+<Route element={<Navigate replace to="/home" />} path="/" />
+<Route path="home" element={<Home />} />
+<Route path="account" element={<Account />} />
+<Route path="login" element={<Login />} />
+<Route path="/logout" element={<Logout />} />
+<Route path="*" element={<Error />} />
+</Routes>
+);
+};
+
+export default Router;
+
