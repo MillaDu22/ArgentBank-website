8e57356c98aff6af09e1e4d2fd82ce5c71b17181
Milla22 <marti.ludmilla@orange.fr>
2023-07-17 16:13:20 +0200

    composant Erreur 404

--- argent-bank/src/router/router.jsx
+++ argent-bank/src/router/router.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Home from "../pages/Home/home";
-//import Error from "../pages/Erreur/erreur";
+import Error from "../pages/Error/Error";
 import Account from "../pages/Account/account";
 import Login from "../pages/Login/login";
 //import Logout from "../pages/Logout/logout";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const Router = () => {
     return (
         <Routes>
-            {/*<Route element={<Navigate replace to="/home" />} path="/" />*/}
+            <Route element={<Navigate replace to="/home" />} path="/" />
             <Route path="/" element={<Home />} />
             <Route path="account" element={<Account />} />
             <Route path="login" element={<Login />} />
-            {/*<Route path="/logout" element={<Logout />} />
-            <Route path="*" element={<Error />} />*/}
+            {/*<Route path="/logout" element={<Logout />} />*/}
+            <Route path="*" element={<Error />} />
         </Routes>
     );
 };
