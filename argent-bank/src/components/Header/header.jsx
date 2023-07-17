96df76b7ca470553c5bc6f330b1200d93a5d52f9
Milla22 <marti.ludmilla@orange.fr>
2023-07-17 15:55:30 +0200

    mise en place useEffect useSelector et useDispatch pour l'affichage des links  dans le navigateur header

--- argent-bank/src/components/Header/header.jsx
+++ argent-bank/src/components/Header/header.jsx
@@ -2,29 +2,62 @@ import React from 'react';
 import Logo from "../../../src/assets/img/argentBankLogo.png";
 import "./header.css";
 import { NavLink } from "react-router-dom";
+import "../../index.css";
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getToken } from "../../features/Token/token";
+import { getLoginFetch } from "../../services/API";
+import { getFirstName } from "../../features/User/firstName";
 
 function Header() {
+    // Use Selector
+    const firstName = useSelector((state) => state.firstName.value);
+    const token = useSelector((state) => state.token.value);
+
+    // Use Effect
+    const dispatch = useDispatch();
+    useEffect(() => {
+    if (token === localStorage.getItem('token')) {
+        dispatch(getToken(localStorage.getItem('token')));
+        const user = getLoginFetch(token);
+        user.then((obj) => {
+            dispatch(getFirstName(obj.firstName));
+        });
+    }
+})
+
 return (
-    <nav className="main-nav">
-        <NavLink to="/" className="main-nav-logo">
-            <img className="main-nav-logo-image" src={Logo} alt="Argent Bank Logo"/>
-            <h1 className="sr-only">Argent Bank</h1>
-        </NavLink>
-        <div>
-        <NavLink to="/login" className="main-nav-item">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-        </NavLink>
-        <NavLink to="/account" className="main-nav-item">
-            <i className="fa fa-user-circle"></i>
-        Nom
-        </NavLink>
-        <NavLink to="/logout" className="main-nav-item">
-            <i className="fa fa-sign-out"></i>
-            Sign Out
-        </NavLink>
-        </div>
-    </nav>
-    );
+<nav className="main-nav">
+    <NavLink to="/" className="main-nav-logo">
+        <img className="main-nav-logo-image" src={Logo} alt="Argent Bank Logo" />
+        <h1 className="sr-only">Argent Bank</h1>
+    </NavLink>
+    <div>
+        {/* Anonyme */}
+        {token === 0 && (
+        <>
+            <NavLink to="/login" className="main-nav-item">
+                <i className="fa fa-user-circle"></i>
+                Sign In
+            </NavLink>
+        </>
+        )}
+
+        {/* Connecté */}
+        {token !== 0 && (
+            <>
+                <NavLink to="/account" className="main-nav-item">
+                    <i className="fa fa-user-circle"></i>
+                    {firstName}
+                </NavLink>
+                <NavLink to="/logout" className="main-nav-item">
+                    <i className="fa fa-sign-out"></i>
+                Sign Out
+                </NavLink>
+            </>
+        )}
+    </div>
+</nav>
+);
 }
 export default Header;
