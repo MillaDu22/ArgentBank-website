f4e3e6d1c3d0116c58c21b1f51de244ee15e0a08
Milla22 <marti.ludmilla@orange.fr>
2023-07-18 16:14:54 +0200

    Maj composant feature dans la homePage

--- argent-bank/src/components/Feature/feature.jsx
+++ argent-bank/src/components/Feature/feature.jsx
@@ -1,15 +1,27 @@
-import "./feature.css";
+import React from 'react';
+import propTypes from 'prop-types';
+import './feature.css';
 
-
-function Feature({imgSrc, imgAlt, titre, description}) {
+/**
+* Creation feature
+* @param { {icon: SVGElement, iconType: String, title: String, description: String} } props – icon: SVG element, iconType: icon description, title: feature title, description: text
+* @returns { React.ReactElement } composant feature
+*/
+function Feature(props) {
     return (
-        <div className="feature-item">
-            <img src={imgSrc} alt={imgAlt} className="feature-icon" />
-            <h3 className="feature-item-title">{titre}</h3>
-            <p>
-                {description}
-            </p>
+        <div className="feature_wrapper">
+            <img className="feature_icon" src={props.icon} alt={props.iconType} />
+            <h3 className="feature_title">{props.title}</h3>
+            <p>{props.description}</p>
         </div>
     );
 }
+
+Feature.propTypes = {
+    icon: propTypes.node.isRequired,
+    iconType: propTypes.string.isRequired,
+    title: propTypes.string.isRequired,
+    description: propTypes.string.isRequired,
+};
+
 export default Feature;
--- argent-bank/src/pages/Home/home.jsx
+++ argent-bank/src/pages/Home/home.jsx
@@ -1,50 +1,35 @@
 import React from 'react';
-import "./home.css";
-import ImageTree from "../../assets/img/bank-tree.jpeg";
-import IconChat from "../../assets/img/icon-chat.png";
-import IconMoney from "../../assets/img/icon-money.png";
-import IconSecurity from "../../assets/img/icon-security.png";
+import chatIcon from '../../assets/img/icon-chat.png';
+import moneyIcon from '../../assets/img/icon-money.png';
+import securityIcon from '../../assets/img/icon-security.png';
+import Feature from '../../components/Feature/feature';
+import './home.css';
 
-const home = () => {
+/**
+* Creation homePage
+* @returns { React.ReactElement } HomePage
+*/
+function Home() {
     return (
-        <div>
-            <main>
-                <section className ="banner-home">
-                    <img src= {ImageTree} className="img-home" alt="pot-pieces"/>
-                        <div className = "text-banner">
-                        <h2 >Promoted Content</h2>
-                        <p >No fees.</p>
-                        <p >No minimum deposit.</p>
-                        <p >High interest rates.</p>
-                        <p >Open a savings account with Argent Bank today!</p>
-                        </div>
-                    </section>
-                <section className="caracteristiques-section">
-                    <h2 >Features</h2>
-                    <div className= "caracteristiques">
-                    <div className="caracteristique">
-                        <img src={IconChat} alt="Chat Icon" className="icon-caracteristique" />
-                        <h3 >You are our #1 priority</h3>
-                        <p>Need to talk to a representative? You can get in touch through our
-                        24/7 chat or through a phone call in less than 5 minutes.
-                        </p>
-                    </div>
-                    <div className="caracteristique">
-                        <img src={IconMoney} alt="Chat Icon" className="icon-caracteristique" />
-                        <h3 >More savings means higher rates</h3>
-                        <p>The more you save with us, the higher your interest rate will be!</p>
-                    </div>
-                    <div className="caracteristique">
-                        <img src={IconSecurity} alt="Chat Icon" className="icon-caracteristique" />
-                        <h3 >Security you can trust</h3>
-                        <p>We use top of the line encryption to make sure your data and money
-                        is always safe.
-                        </p>
-                    </div>
-                    </div>
+        <main className="home_wrapper">
+            <div className="hero_wrapper">
+                <section className="hero_content">
+                    <h2 className="sr-only">Promoted Content</h2>
+                    <p className="hero_subtitle">No fees.</p>
+                    <p className="hero_subtitle">No minimum deposit.</p>
+                    <p className="hero_subtitle">High interest rates.</p>
+                    <p className="hero_text">Open a savings account with Argent Bank today!</p>
                 </section>
-            </main>
-        </div>
+            </div>
+            <section className="features">
+                <h2 className="sr-only">Features</h2>
+                <Feature icon={chatIcon} iconType="Chat icon" title="You are our #1 priority" description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."/>
+                <Feature icon={moneyIcon} iconType="Money icon" title="More savings means higher rates" description="The more you save with us, the higher your interest rate will be!" />
+                <Feature icon={securityIcon} iconType="Security icon" title="Security you can trust" description="We use top of the line encryption to make sure your data and money is always safe."/>
+            </section>
+        </main>
     );
-};
-export default home;
+}
+
+export default Home;
+
